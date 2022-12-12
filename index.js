dbba36907bccfdef20e6677edd2bec4788e36712
Noah Weston <nsweston24@gmail.com>
2022-12-12 21:09:43 +0000

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+//this is my first javascript code
+console.log('Hello World');
+
+let name = 'Mosh';
+console.log(name);
+
+//cannot be reserved keyword
+//should be meaningful
+//cannot start with number
+//no spaces or hyphens.,
